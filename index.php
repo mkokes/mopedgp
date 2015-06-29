365518220e0c0d8e4e616a8b8c93a416098c38c4
Marty Kokes <mkokes@gmail.com>
2015-06-28 21:23:33 -0500

    starting on frontpage and off canvas menu

--- index.php
+++ index.php
@@ -5,21 +5,34 @@
 	<meta name="viewport" content="initial-scale=1.0, width=device-width, height=device-height, minimum-scale=1.0, maximum-scale=1.0, user-scalable=no" />
 	<link rel="profile" href="http://gmpg.org/xfn/11">
 	<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
-	<title>Document</title>
+	<title><?php wp_title();?></title>
 	<?php wp_head();?>
 </head>
 <body <?php body_class();?>>
 
 
 	<div id="wrapper">
-		<div id="offcanvas"></div>
+		<div id="offcanvas">
+
+		</div>
 		<div id="canvas">
-			<header>
+
+			<header id="site-header">
 			</header>
-			<footer>
+			<div class="page">
+				<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+				<article class="post">
+					<h1 class="post-title"><?php the_title();?></h1>
+					<p class="story-excerpt"><?php the_excerpt();?></p>
+				</article>
+				<?php endwhile; else : ?>
+					<p><?php _e( 'Sorry, no posts matched your criteria.' ); ?></p>
+				<?php endif; ?>
+			</div>
+			<footer id="site-footer">
+				<?php wp_footer();?>
 			</footer>
 		</div>
 	</div>
-
 </body>
 </html>
