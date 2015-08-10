b1ba75bfe4a5fd47478f2df013daa3367ca404cd
Marty Kokes <mkokes@gmail.com>
2015-08-10 16:58:36 -0500

    Switched elements from ids to classes

--- index.php
+++ index.php
@@ -16,7 +16,7 @@
 
 
 		<div id="wrapper">
-			<div id="offcanvas">
+			<div class="offcanvas">
 				<?php if ( has_nav_menu( 'offcanvas' ) ) {
 					wp_nav_menu( array(
 						'theme_location' => 'offcanvas',
@@ -26,43 +26,47 @@
 						'walker' => new My_Sub_Menu()) );
 			} ?>
 			</div>
-			<div id="canvas">
-				<header id="site-header">
-					<div id="top-navicon">
-						<div class="nav-icon">
-							<div></div>
+			<div class="canvas">
+				<div class="inner-canvas">
+
+					<header class="site-header">
+						<div class="top-navicon">
+							<div class="nav-icon">
+								<div></div>
+							</div>
 						</div>
-					</div>
-					<div id='top-sitelogo'>
-						<a href='<?php echo esc_url( home_url( ' / ' ) ); ?>' title='<?php echo esc_attr( get_bloginfo( ' name ', 'display ' ) ); ?>' rel='home'>
+						<div id='top-sitelogo'>
+							<a href='<?php echo esc_url( home_url( ' / ' ) ); ?>' title='<?php echo esc_attr( get_bloginfo( ' name ', 'display ' ) ); ?>' rel='home'>
 						<img src="<?php echo get_stylesheet_directory_uri(); ?>/img/logo.png">
 					</a>
-					</div>
-					<!--<div id="top-sitesearch">
+						</div>
+						<!--<div id="top-sitesearch">
 					<?php get_search_form( ); ?>
 				</div>-->
-				</header>
-				<div class="page">
-					<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
-						<article class="post">
-							<h1 class="post-title">
-								<?php the_title();?>
-							</h1>
-							<p class="story-excerpt">
-								<?php the_excerpt();?>
-							</p>
-						</article>
-						<?php endwhile; else : ?>
-							<p>
-								<?php _e( 'Sorry, no posts matched your criteria.' ); ?>
-							</p>
-							<?php endif; ?>
+					</header>
+					<div class="page">
+						<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+							<article class="post">
+								<h1 class="post-title">
+									<?php the_title();?>
+								</h1>
+								<p class="story-excerpt">
+									<?php the_excerpt();?>
+								</p>
+							</article>
+							<?php endwhile; else : ?>
+								<p>
+									<?php _e( 'Sorry, no posts matched your criteria.' ); ?>
+								</p>
+								<?php endif; ?>
+					</div>
+					<footer id="site-footer">
+						<div class="footer-left">&copy;
+							<?php echo date("Y"); ?> MopedGP</div>
+						<div class="footer-right"></div>
+						<?php wp_footer();?>
+					</footer>
 				</div>
-				<footer id="site-footer">
-					<div class="footer-left">&copy; <?php echo date("Y"); ?> MopedGP</div>
-					<div class="footer-right"></div>
-					<?php wp_footer();?>
-				</footer>
 			</div>
 		</div>
 	</body>
--- js/mopedgp.js
+++ js/mopedgp.js
@@ -1,44 +1,45 @@
-jQuery(document).ready(function($) {
+jQuery( document ).ready( function ( $ ) {
 
   //Canvas, Offcanvas and Navicon Behavior
-  $('.nav-icon').click(function() {
-    $('#canvas').toggleClass('open');
-    $('#offcanvas').toggleClass('open');
-    $('#logobar').toggleClass('open');
-  });
+  $( '.nav-icon' ).click( function () {
+    $( '.inner-canvas' ).toggleClass( 'open' );
+    $( '.offcanvas' ).toggleClass( 'open' );
+    $( '.logobar' ).toggleClass( 'open' );
+  } );
 
-  $('.nav-description').click(function() {
-    $('#canvas').toggleClass('open');
-    $('#offcanvas').toggleClass('open');
-    $('#logobar').toggleClass('open');
-  });
+  $( '.nav-description' ).click( function () {
+    $( '.inner-canvas' ).toggleClass( 'open' );
+    $( '.offcanvas' ).toggleClass( 'open' );
+    $( '.logobar' ).toggleClass( 'open' );
+  } );
 
-  $('#offcanvas').find('.sub-menu').hide();
-  $('.menu-item-has-children').click(function(event) {
+  $( '.offcanvas' ).find( '.sub-menu' ).hide();
+  $( '.menu-item-has-children' ).click( function ( event ) {
     event.stopPropagation();
-    $(this).children('.sub-menu').slideToggle();
-    $(this).toggleClass('accordian');
-  });
+    $( this ).children( '.sub-menu' ).slideToggle();
+    $( this ).toggleClass( 'accordian' );
+  } );
 
   //Scroll to top button and fixed menu switch
-  $(window).scroll(function(event) {
-    var y = $(this).scrollTop();
-    if (y >= 500) {
-      $("#scrolltotop").addClass("show");
+  $( window ).scroll( function ( event ) {
+    var y = $( this ).scrollTop();
+    if ( y >= 500 ) {
+      $( '.scrolltotop' ).addClass( 'show' );
     } else {
-      $("#scrolltotop").removeClass("show");
+      $( '.scrolltotop' ).removeClass( 'show' );
     }
-    if (y >= 100) {
-      $("#logobar").addClass("fixed");
-    } else {
-      $("#logobar").removeClass("fixed");
-    }
-  });
 
-  $('#scrolltotop').click(function() {
-    $("html, body").animate({
-      scrollTop: 0
-    }, "slow");
+    if ( y >= 100 ) {
+      $( '.logobar' ).addClass( 'fixed' );
+    } else {
+      $( '.logobar' ).removeClass( 'fixed' );
+    }
+  } );
+
+  $( '.scrolltotop' ).click( function () {
+    $( 'html, body' ).animate( {
+      scrollTop: 0,
+    }, 'slow' );
     return false;
-  });
-});
+  } );
+} );
