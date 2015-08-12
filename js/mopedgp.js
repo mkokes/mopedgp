71ecdc31207209fa341ab04302f3fc073f3b589f
Marty Kokes <mkokes@gmail.com>
2015-08-12 12:10:05 -0500

    fixed navicon toggle and beautified

--- js/mopedgp.js
+++ js/mopedgp.js
@@ -1,45 +1,45 @@
-jQuery( document ).ready( function ( $ ) {
+jQuery(document).ready(function ($) {
 
   //Canvas, Offcanvas and Navicon Behavior
-  $( '.nav-icon' ).click( function () {
-    $( '.inner-canvas' ).toggleClass( 'open' );
-    $( '.offcanvas' ).toggleClass( 'open' );
-    $( '.logobar' ).toggleClass( 'open' );
-  } );
+  $('.nav-icon').click(function () {
+    $('.canvas').toggleClass('open');
+    $('.offcanvas').toggleClass('open');
+    $('.top-navicon').toggleClass('open');
+  });
 
-  $( '.nav-description' ).click( function () {
-    $( '.inner-canvas' ).toggleClass( 'open' );
-    $( '.offcanvas' ).toggleClass( 'open' );
-    $( '.logobar' ).toggleClass( 'open' );
-  } );
+  $('.nav-description').click(function () {
+    $('.inner-canvas').toggleClass('open');
+    $('.offcanvas').toggleClass('open');
+    $('.top-navicon').toggleClass('open');
+  });
 
-  $( '.offcanvas' ).find( '.sub-menu' ).hide();
-  $( '.menu-item-has-children' ).click( function ( event ) {
+  $('.offcanvas').find('.sub-menu').hide();
+  $('.menu-item-has-children').click(function (event) {
     event.stopPropagation();
-    $( this ).children( '.sub-menu' ).slideToggle();
-    $( this ).toggleClass( 'accordian' );
-  } );
+    $(this).children('.sub-menu').slideToggle();
+    $(this).toggleClass('accordian');
+  });
 
   //Scroll to top button and fixed menu switch
-  $( window ).scroll( function ( event ) {
-    var y = $( this ).scrollTop();
-    if ( y >= 500 ) {
-      $( '.scrolltotop' ).addClass( 'show' );
+  $(window).scroll(function (event) {
+    var y = $(this).scrollTop();
+    if(y >= 500) {
+      $('.scrolltotop').addClass('show');
     } else {
-      $( '.scrolltotop' ).removeClass( 'show' );
+      $('.scrolltotop').removeClass('show');
     }
 
-    if ( y >= 100 ) {
-      $( '.logobar' ).addClass( 'fixed' );
+    if(y >= 100) {
+      $('.logobar').addClass('fixed');
     } else {
-      $( '.logobar' ).removeClass( 'fixed' );
+      $('.logobar').removeClass('fixed');
     }
-  } );
+  });
 
-  $( '.scrolltotop' ).click( function () {
-    $( 'html, body' ).animate( {
+  $('.scrolltotop').click(function () {
+    $('html, body').animate({
       scrollTop: 0,
-    }, 'slow' );
+    }, 'slow');
     return false;
-  } );
-} );
+  });
+});
