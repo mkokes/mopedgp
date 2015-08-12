9ca495a903f43dec70bde72fb6a327050b1619b5
Marty Kokes <mkokes@gmail.com>
2015-08-12 18:30:41 -0500

    added advanced custom fields to theme

--- components/customfields.php
+++ components/customfields.php
@@ -28,6 +28,41 @@ function cptui_register_my_cpts() {
 // End of cptui_register_my_cpts()
 }
 //Advanced Custom Fields
+	//Include in theme
+	// 1. customize ACF path
+add_filter('acf/settings/path', 'my_acf_settings_path');
+
+function my_acf_settings_path( $path ) {
+
+    // update path
+    $path = get_stylesheet_directory() . '/plugins/advanced-custom-fields/';
+
+    // return
+    return $path;
+
+}
+
+
+// 2. customize ACF dir
+add_filter('acf/settings/dir', 'my_acf_settings_dir');
+
+function my_acf_settings_dir( $dir ) {
+
+    // update path
+    $dir = get_stylesheet_directory_uri() . '/plugins/advanced-custom-fields/';
+
+    // return
+    return $dir;
+
+}
+
+
+// 3. Hide ACF field group menu item
+add_filter('acf/settings/show_admin', '__return_false');
+
+
+// 4. Include ACF
+include_once( get_stylesheet_directory() . '/plugins/advanced-custom-fields/acf.php' );
 if(function_exists("register_field_group"))
 {
 	register_field_group(array (
