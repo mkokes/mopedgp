cfffb0fba1d3695ec5b25ee3e484fa0d1f1b1e7a
Marty Kokes <mkokes@gmail.com>
2015-08-13 12:25:05 -0500

    added front page

---
+++ front-page.php
@@ -0,0 +1,22 @@
+<?php get_header(); ?>
+				<div class="inner-canvas">
+
+					<div class="page">
+						<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+							<article class="post">
+								<h1 class="post-title">
+									<a href="<?php the_permalink();?>" title="<?php the_title();?>"><?php the_title();?></a>
+								</h1>
+								<p class="story-excerpt">
+									<?php the_excerpt();?>
+								</p>
+							</article>
+							<?php endwhile; else : ?>
+								<p>
+									<?php _e( 'Sorry, no posts matched your criteria.' ); ?>
+								</p>
+								<?php endif; ?>
+					</div>
+
+				</div>
+<?php get_footer(); ?>
